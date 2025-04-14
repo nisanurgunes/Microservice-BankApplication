44210cdf194becee2db73334e058da95574ce209
nisanurgunes <77933400+nisanurgunes@users.noreply.github.com>
2025-04-14 17:20:27 +0300

    Completed Spring Cloud Configuration with using file-based.

--- loans/src/main/java/com/bank/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/bank/loans/dto/LoansContactInfoDto.java
@@ -13,4 +13,28 @@ public class LoansContactInfoDto {
   private String message;
   private Map<String, String> contactDetails;
   private List<String> onCallSupport;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public Map<String, String> getContactDetails() {
+    return contactDetails;
+  }
+
+  public void setContactDetails(Map<String, String> contactDetails) {
+    this.contactDetails = contactDetails;
+  }
+
+  public List<String> getOnCallSupport() {
+    return onCallSupport;
+  }
+
+  public void setOnCallSupport(List<String> onCallSupport) {
+    this.onCallSupport = onCallSupport;
+  }
 }
